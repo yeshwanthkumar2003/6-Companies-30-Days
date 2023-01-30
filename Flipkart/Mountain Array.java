c3b9c8f078829e586ab9f5c8ee421822314583b7
yeshwanth <94180456+yeshwanthkumar2003@users.noreply.github.com>
2023-01-30 09:41:10 +0530

    Create Mountain Array.java

---
+++ Flipkart/Mountain Array.java
@@ -0,0 +1,53 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        // Binary Search
+
+    int n = mountainArr.length(), l = 0, r = n - 1, mid, peak_pos;
+    // Find peak position
+    while (l < r) {
+        mid = l + (r - l) / 2;
+        if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+            l = peak_pos = mid + 1;
+        } else {
+            r = mid;
+        }
+    }
+    // Find left part
+    l = 0;
+
+    peak_pos = r;
+    while (l <= r) {
+        mid = l + (r - l) / 2;
+        if (mountainArr.get(mid) < target) {
+            l = mid + 1;
+        } else if (mountainArr.get(mid) > target) {
+            r = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    // Find right part
+    l = peak_pos;
+    r = n - 1;
+    while (l <= r) {
+        mid = l + (r - l) / 2;
+        if (mountainArr.get(mid) > target) {
+            l = mid + 1;
+        } else if (mountainArr.get(mid) < target) {
+            r = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+    }
