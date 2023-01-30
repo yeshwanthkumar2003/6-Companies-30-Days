50318ffbe121d072adb9efe5c462450ad7c8b3f4
yeshwanth <94180456+yeshwanthkumar2003@users.noreply.github.com>
2023-01-30 09:28:35 +0530

    Create increasing triplets.py

---
+++ Adobe/increasing triplets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        nums_i = float('inf')
+        nums_j = float('inf')
+
+        for num in nums:
+            if(num<=nums_i):
+                nums_i = num 
+            elif(num<=nums_j):
+                nums_j = num 
+            else:
+                return True 
+        return False
