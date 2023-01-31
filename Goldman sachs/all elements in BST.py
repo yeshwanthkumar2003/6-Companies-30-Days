83dc0eb64a77d600db9b6718acf44955dd249833
yeshwanth <94180456+yeshwanthkumar2003@users.noreply.github.com>
2023-01-31 15:16:18 +0530

    Create all elements in BST.py

---
+++ Goldman sachs/all elements in BST.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        def dfs(root):
+            if not root: return []
+            return dfs(root.right) + [root.val] + dfs(root.left)
+
+        el1, el2, res = dfs(root1), dfs(root2), []       
+        while el1 and el2:                
+            res.append(el1.pop() if el1[-1] < el2[-1] else el2.pop())
+
+        return res + list(reversed(el1 or el2))
