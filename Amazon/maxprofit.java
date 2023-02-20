a217a3a806d888f6e6d60931d8b1c5d8c91b591b
yeshwanth <94180456+yeshwanthkumar2003@users.noreply.github.com>
2023-02-20 11:50:19 +0530

    Create maxprofit.java

---
+++ Amazon/maxprofit.java
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int maxProfit(int K, int N, int A[]) {
+
+        // DP of Transaction Count vs Price
+        vector<vector<int>> TP(K+1, vector<int>(N+1, 0));
+
+        for(int t = 1; t <= K; ++t) {
+            for(int p = 1; p <= N; ++p) {
+                TP[t][p] = TP[t][p - 1];
+                for(int i = 0; i < p; ++i) {
+                    TP[t][p] = max(TP[t][p], TP[t-1][i] + A[p-1] - A[i]);
+                }
+            }
+        }
+        return TP.back().back();
+    }
+};
